f4782145331ad577e9f58513241039a14f057ed9
Murat Tekinok <tekinokmurat@gmail.com>
2023-12-12 15:07:28 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer'; 
+import './App.css'; // Import the App.css file
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <section className="content">
+        <h2>Welcome to Our E-Commerce Website</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ac feugiat purus...</p>
+        {/* Rest of the content */}
+      </section>
+      <Footer>
+        <p>© 2023 My Website. All rights reserved.</p>
+      </Footer>
     </div>
   );
-}
+};
 
 export default App;
