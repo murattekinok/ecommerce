a08b5765c5e660d7a6a7bca1e1066172395192db
Murat Tekinok <tekinokmurat@gmail.com>
2023-12-12 15:33:33 +0100

    copying from old project

--- src/App.js
+++ src/App.js
@@ -2,20 +2,18 @@
 import React from 'react';
 import Header from './Header';
 import Footer from './Footer'; 
+import Main from './Main' ;
+import Features from './Features' ;
 import './App.css'; // Import the App.css file
 
 const App = () => {
   return (
     <div>
       <Header />
-      <section className="content">
-        <h2>Welcome to Our E-Commerce Website</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ac feugiat purus...</p>
-        {/* Rest of the content */}
-      </section>
-      <Footer>
-        <p>© 2023 My Website. All rights reserved.</p>
-      </Footer>
+      <Main/>
+      <Features/>
+      <Footer/>
+        
     </div>
   );
 };
